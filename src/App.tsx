216f24c2b7d1c2090482902bc5ebc58ba90bd910
stressgc <17021662+stressGC@users.noreply.github.com>
2022-08-19 10:27:20 +0200

    feat: support swiping events

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { use2048 } from "./hooks/use2048"
 import { Grid } from "./components/Grid/Grid"
 
 export function App() {
-	const { score, grid, isDefeated } = use2048()
+	const { score, grid, isDefeated, handleGridMove } = use2048()
 
 	return (
 		<div className="w-screen h-screen flex items-center justify-center">
 			<div>
 				<h1 className="text-4xl">{score}</h1>
-				{isDefeated ? <h2 className="text-4xl">Defeat!</h2> : <Grid grid={grid} />}
+				{isDefeated ? (
+					<h2 className="text-4xl">Defeat!</h2>
+				) : (
+					<Grid handleGridMove={handleGridMove} grid={grid} />
+				)}
 			</div>
 		</div>
 	)
--- src/hooks/use2048.ts
+++ src/hooks/use2048.ts
@@ -1,5 +1,5 @@
 import isEqual from "lodash/isEqual"
-import { useKeyPressEvent, useStateWithHistory } from "react-use"
+import { useStateWithHistory } from "react-use"
 import { injectRandomNewCellWhereEmpty } from "../lib/injectRandomNewCellWhereEmpty"
 import { recomputeGridForMove } from "../lib/recomputeGridForMove"
 import { computeScore } from "../lib/computeScore"
@@ -16,30 +16,12 @@ function makeRandomStartingGrid() {
 	return injectRandomNewCellWhereEmpty(injectRandomNewCellWhereEmpty(empty4x4Grid))
 }
 
-type BindedKeyboardKey = "ArrowUp" | "ArrowDown" | "ArrowLeft" | "ArrowRight"
-function isBindedKeyboardKey(key: string): key is BindedKeyboardKey {
-	const bindedKeyboardKeys = ["ArrowUp", "ArrowDown", "ArrowLeft", "ArrowRight"]
-	return bindedKeyboardKeys.includes(key)
-}
-
 export type MoveDirection = "top" | "bottom" | "left" | "right"
-
-const eventKeyToMoveDirectionMapping: Record<BindedKeyboardKey, MoveDirection> = {
-	ArrowUp: "top",
-	ArrowDown: "bottom",
-	ArrowLeft: "left",
-	ArrowRight: "right",
-}
 
 function useGrid() {
 	const [grid, setGrid] = useStateWithHistory(makeRandomStartingGrid(), 20)
 
-	function onKeyPress(event: KeyboardEvent) {
-		if (!isBindedKeyboardKey(event.key)) {
-			return
-		}
-		const moveDirection = eventKeyToMoveDirectionMapping[event.key]
-
+	function handleGridMove(moveDirection: MoveDirection) {
 		const recomputedGrid = recomputeGridForMove(grid, moveDirection)
 		if (isEqual(grid, recomputedGrid)) return
 
@@ -47,19 +29,14 @@ function useGrid() {
 		setGrid(recomputedGridWithRandomNewCell)
 	}
 
-	useKeyPressEvent("ArrowUp", onKeyPress)
-	useKeyPressEvent("ArrowDown", onKeyPress)
-	useKeyPressEvent("ArrowLeft", onKeyPress)
-	useKeyPressEvent("ArrowRight", onKeyPress)
-
-	return grid
+	return { grid, handleGridMove }
 }
 
 export function use2048() {
-	const grid = useGrid()
+	const { grid, handleGridMove } = useGrid()
 
 	const score = computeScore(grid)
 	const isDefeated = checkForDefeat(grid)
 
-	return { grid, score, isDefeated }
+	return { grid, score, isDefeated, handleGridMove }
 }
